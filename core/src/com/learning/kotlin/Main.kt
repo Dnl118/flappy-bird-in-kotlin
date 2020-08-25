1f317817e02c59b177fb46ce587a93bb82ed6c5b
Danilo de Sousa <danspaulo@gmail.com>
2020-08-24 21:30:55 -0300

    Trying to render a bird but without success.

--- core/src/com/learning/kotlin/Main.kt
+++ core/src/com/learning/kotlin/Main.kt
@@ -1,30 +1,26 @@
 package com.learning.kotlin
 
 import com.badlogic.gdx.ApplicationAdapter
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 class Main : ApplicationAdapter() {
-    var batch: SpriteBatch? = null
-    var img: Texture? = null
+
+    private var batch: SpriteBatch? = null
+    private var bird: Texture? = null
 
     override fun create() {
         batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        bird = Texture("passaro1.png")
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         batch!!.begin()
-        batch!!.draw(img, 0f, 0f)
+        batch!!.draw(bird!!, 300f, 50f)
         batch!!.end()
     }
 
     override fun dispose() {
-        batch!!.dispose()
-        img!!.dispose()
+
     }
 }
