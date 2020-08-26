54a78d382c24e7b8a7222d0d7b824a5b21ec157e
Danilo de Sousa <danspaulo@gmail.com>
2020-08-26 20:11:33 -0300

    Adding background and moving bird ahead.

--- core/src/com/learning/kotlin/Main.kt
+++ core/src/com/learning/kotlin/Main.kt
@@ -2,6 +2,7 @@ package com.learning.kotlin
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
@@ -9,15 +10,32 @@ class Main : ApplicationAdapter() {
 
     private lateinit var batch: SpriteBatch
     private lateinit var bird: Texture
+    private lateinit var background: Texture
+
+    private var birdPositionX = 0f
+
+    private var screenWidth = 0f
+    private var screenHeight = 0f
 
     override fun create() {
+        screenWidth = Gdx.graphics.width.toFloat()
+        screenHeight = Gdx.graphics.height.toFloat()
+
         batch = SpriteBatch()
         bird = Texture("passaro1.png")
+        background = Texture("fundo.png")
     }
 
     override fun render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
         batch.begin()
-        batch.draw(bird, 300f, 50f)
+
+        batch.draw(background,0f, 0f, screenWidth, screenHeight)
+        batch.draw(bird, birdPositionX, 500f)
+
+        birdPositionX += 2.5f
+
         batch.end()
     }
 
