87164eb828c55497cbfdb09dfa4c996a1cac9b64
Danilo de Sousa <danspaulo@gmail.com>
2020-08-28 20:50:50 -0300

    - Bird animation. - Click events for jumps. - Gravity.

--- core/src/com/learning/kotlin/Main.kt
+++ core/src/com/learning/kotlin/Main.kt
@@ -9,37 +9,80 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 class Main : ApplicationAdapter() {
 
     private lateinit var batch: SpriteBatch
-    private lateinit var bird: Texture
     private lateinit var background: Texture
 
-    private var birdPositionX = 0f
+    private lateinit var birds: Array<Texture>
+
+    private var birdPositionX = 30f
+    private var birdPositionY = 0f
 
     private var screenWidth = 0f
     private var screenHeight = 0f
+
+    private var animationIndex = 0f
+
+    private var gravity = 0f
+
+    private var justTouched = false
 
     override fun create() {
         screenWidth = Gdx.graphics.width.toFloat()
         screenHeight = Gdx.graphics.height.toFloat()
 
+        birdPositionY = screenHeight / 2
+
         batch = SpriteBatch()
-        bird = Texture("passaro1.png")
+
+        birds = arrayOf(Texture("passaro1.png"),
+                Texture("passaro2.png"),
+                Texture("passaro3.png"))
+
         background = Texture("fundo.png")
     }
 
     override fun render() {
+        justTouched = Gdx.input.justTouched()
+
+        if (justTouched) {
+            gravity = -25f
+        }
+
+        applyGravity()
+
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
         batch.begin()
 
-        batch.draw(background,0f, 0f, screenWidth, screenHeight)
-        batch.draw(bird, birdPositionX, 500f)
-
-        birdPositionX += 2.5f
+        batch.draw(background, 0f, 0f, screenWidth, screenHeight)
+        drawBird()
 
         batch.end()
+
+        calculatePhysics()
     }
 
     override fun dispose() {
+    }
 
+    private fun drawBird() {
+        if (animationIndex >= birds.size) {
+            animationIndex = 0f
+        }
+
+        if (batch.isDrawing) {
+            batch.draw(birds[animationIndex.toInt()], birdPositionX, birdPositionY)
+        }
+
+        animationIndex += Gdx.app.graphics.deltaTime * 10
+    }
+
+    private fun applyGravity() {
+        if (birdPositionY > 0 || justTouched) {
+            birdPositionY -= gravity
+        }
+    }
+
+    private fun calculatePhysics() {
+        gravity ++
     }
 }
